85bd9cf95fc3a1f709e257dbae95cd4bea29157a
abocoum <abocoum@hotmail.fr>
2013-02-21 13:22:11 +0100

    Add nothinf

--- src/com/hubuu/nerfacier/JouerActivitity.java
+++ src/com/hubuu/nerfacier/JouerActivitity.java
@@ -7,7 +7,9 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class JouerActivitity extends Activity implements SensorEventListener {
 
@@ -16,13 +18,18 @@ public class JouerActivitity extends Activity implements SensorEventListener {
 	private Sensor mAccelerometer;
 	private float[] m_dOrentationValues = new float[3];
 	
+	private float[] m_dOldOrentationValues = new float[20];
+	private int currentIndex=0;
+	
+	private float depart;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_jouer_activitity);
 
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor. TYPE_ORIENTATION );
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
@@ -37,6 +44,7 @@ public class JouerActivitity extends Activity implements SensorEventListener {
 		super.onResume();
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 	}
+
 	protected void onPause() {
 		super.onPause();
 		mSensorManager.unregisterListener(this);
@@ -53,8 +61,18 @@ public class JouerActivitity extends Activity implements SensorEventListener {
 	public void onSensorChanged(SensorEvent event) {
 		// TODO Auto-generated method stub
 		if (event.sensor.getType() == Sensor.TYPE_ORIENTATION ) {
+			TextView tv = (TextView)findViewById(R.id.textview);
+
+			tv.setText("X: "+event.values[0]+"\n "+"Y: "+event.values[1]+"\n "+"Z: "+event.values[2]+"\n" );
 			
+			m_dOrentationValues[0] = event.values[0];
+			m_dOrentationValues[1] = event.values[1];
+			m_dOrentationValues[2] = event.values[2];
 			
+			if(m_dOrentationValues[1] <0 ){
+//				sm_dOldOrentationValues[];
+			}
+			Log.v("man", "end MAin");
 		}
 	}
 
