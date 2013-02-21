e776ef4171023306a469d242a5cebcbb9de19f6e
abocoum <abocoum@hotmail.fr>
2013-02-21 17:24:58 +0100

    fcff

--- gen/com/hubuu/nerfacier/R.java
+++ gen/com/hubuu/nerfacier/R.java
@@ -24,14 +24,16 @@ public final class R {
         public static final int activity_main=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_before_play=0x7f060000;
-        public static final int activity_jouer_activitity=0x7f060001;
-        public static final int activity_main=0x7f060002;
+        public static final int activity_avant_jouer=0x7f060000;
+        public static final int activity_before_play=0x7f060001;
+        public static final int activity_jouer_activitity=0x7f060002;
+        public static final int activity_main=0x7f060003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_avant_jouer=0x7f040005;
         public static final int title_activity_before_play=0x7f040004;
         public static final int title_activity_jouer_activitity=0x7f040003;
     }
--- src/com/hubuu/nerfacier/CustomCircleView.java
+++ src/com/hubuu/nerfacier/CustomCircleView.java
@@ -4,125 +4,73 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 
 public class CustomCircleView extends View  {
 
-	private Paint paintExt;
+	 
 	Paint paintInt;
-	private ShapeDrawable mDrawable ;
-	private ShapeDrawable mDrawableForBIg ;
 	private Cercle cercleInt ;
-	private Cercle cercleExt;
+	private float maxrange=1;
 	
 
-	public CustomCircleView(Context context) {
-		super(context);
-		Log.v("Custumview", "Version1");
-		mDrawable = new ShapeDrawable(new OvalShape());
-		mDrawable.getPaint().setColor(0xff74AC23);
+	 
 
-	}
-
-	public CustomCircleView(Cercle cercle, Context context) {
-
-		super(context);
-		Log.v("Custumview", "Version2");
-		mDrawable = new ShapeDrawable(new OvalShape());
-		mDrawable.getPaint().setColor(0xff74AC23);
-
-	}
+	 
 
 	public CustomCircleView(Context context, AttributeSet attr) {
 		super(context, attr);
 		 Init();
+		 Log.v("init", "in init");
 		
 	}
 
 
 	private void Init (){		 		 		  
-//		mDrawable = new ShapeDrawable(new OvalShape()); // ici on affiche un oval...
-//		mDrawable.getPaint().setColor(Color.  GREEN);
-//		mDrawable.getPaint().setStyle(Paint.Style.FILL);
-//		//mDrawable.setBounds(100, 100, 300, 300); 
-//		
-//		//
-//		mDrawableForBIg = new ShapeDrawable(new OvalShape()); // ici on affiche un oval...
-//		mDrawableForBIg.getPaint().setColor(Color. BLUE);
-//		mDrawableForBIg.getPaint().setStyle(Paint.Style. STROKE);
-//		mDrawableForBIg.getPaint().setStrokeWidth(2);
-//		//mDrawableForBIg.setBounds(100, 100, 500, 500); 
 		
-		cercleExt= new Cercle();
+		 
 		cercleInt = new Cercle();
 		
 		paintInt = new Paint();
-		paintInt.setColor(Color.  GREEN);
+		paintInt.setColor(Color.GREEN);
 		paintInt.setStyle(Paint.Style.FILL);
-		
-		paintExt  = new Paint();
-		paintExt.setColor(Color.BLUE);
-		paintExt.setStyle(Paint.Style.STROKE);		
-		paintExt.setStrokeWidth(2);
-		
-		
+		Log.v("init", "in init");
 		}
 	
 	protected void onDraw(Canvas canvas) {		
 		super.onDraw(canvas);
 		Log.v("CV", "in onDraw");
-		canvas.drawCircle(cercleExt.getX(),cercleExt.getY(),cercleExt.getRayon(),paintExt);
-		canvas.drawCircle(cercleInt.getX(),cercleInt.getY(),cercleInt.getRayon(),paintInt);			
+		
+		
+		int xCenter = this.getWidth() / 2, yCenter =  getHeight() / 2;
+		
+		
+		
+		float xToDisp = (cercleInt.getX() * 100) / maxrange;
+		float yToDisp = (cercleInt.getY() * 100) / maxrange;
+
+		// then draw the point using 3 circles (and translate the coordonates according to the
+		// center of the screen
+		canvas.drawCircle(xCenter - xToDisp, yToDisp + yCenter, cercleInt.getRayon(), paintInt);
+//		canvas.drawCircle(cercleInt.getX(),cercleInt.getY(),cercleInt.getRayon(),paintInt);			
 	}
 		
-
-	
-
-
-	public Cercle getCercleExt() {
-		return cercleExt;
-
-	}
-
-
-	public void setCercleExt(Cercle cercle) {
-		this.cercleExt = cercle;
-		this.invalidate();
-		requestLayout();
-	}
-
-	
 	public Cercle getCercleInt() {
 		return cercleInt;
 
 	}
 
 
-	public void setCercleInt(Cercle cercle) {
+	public void setCercleInt(Cercle cercle, float maxrange) {
 		this.cercleInt = cercle;
-	}
-
-	public boolean onTouchEvent( MotionEvent arg1) {
-//		switch (arg1.getAction()) {
-//		case MotionEvent.ACTION_MOVE :
-//		{
-//			paintInt.setColor(Color.GRAY);                
-//			invalidate(); // pour invalider l'image et forcer un rappel à la methode onDraw de la classe.
-//		}
-//		}
-		paintInt.setColor(Color. MAGENTA);
-		Log.v("ontouch", "paitn");
-		
+		this.maxrange= maxrange;
 		this.invalidate();
-
-		return false;
+		requestLayout();
 	}
+
+ 
 
 
 }
